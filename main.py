6ba286689eec629732047cd3a39ca6497c14a507
UVRS <aldarizky36@gmail.com>
2023-09-06 11:56:18 +0700

    3 sintaksis dasar

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ ini adalah project pertama saya dengan python
 """
 print("hello world")
 print("My name is Alda Rizky Sefa")
+
+# Percabangan
+ingin_cepat = False
+
+if ingin_cepat:
+    print('Jalan lurus aja ya !')
+else:
+    print('Gunakan alternatif')
+
+# Perulangan
+Jumlah_anak = 4
+for index_anak in range(1, Jumlah_anak+1):
+    print(f'hello anak#{index_anak}')
---
+++ test sequential.py
@@ -0,0 +1,12 @@
+"""
+sintaksis dasar bahasa pemrograman terdiri dari ;
+1 sekuensial ; langkah berurutan
+2 percabangan ; langkah melompat jika kondisi terpenuhi
+3 perulangan ; mengulang langkah yang sama berulang kali selama/sampai kondisi terpenuhi
+"""
+
+print('Ibu berkata, "Pergilah ke toko"')
+print('Tono menjawab, "Baik, apa yang harus saya lakukan?"')
+print('Ibu berkata, "Belikan satu botol susu,dan jika ada telur, belikan telur 6 butir"')
+print("tono berangkat ke toko")
+print(5*6)
